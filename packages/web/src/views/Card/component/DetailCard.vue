3d669f7b858efeb4d898c169dcba632aec8003cd
liyeping <1498328576@qq.com>
2023-07-03 14:07:51 +0800

    修改中英文及滚动条样式

--- packages/web/src/views/Card/component/DetailCard.vue
+++ packages/web/src/views/Card/component/DetailCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="detail Scroll-bar" :class="{'isNight':!isDay}">
+    <div class="detail" :class="{'isNight':!isDay}">
         <!-- 头部时间 -->
         <Header :foreCastInfo="foreCastInfo" />
         <!-- 未来几天天气 -->
@@ -36,7 +36,9 @@ const props = defineProps<{
     align-items: flex-start;
     flex-direction: column;
     max-width:457px;
+    height: 100vh;
     min-height: 100vh;
+    overflow: auto;
     background: url(../../../assets/img/day_bg.png) no-repeat;
     background-size: 100% 100%;
     padding: 0 12px;
@@ -47,19 +49,19 @@ const props = defineProps<{
     background: url(../../../assets/img/night_bg.png) no-repeat;
     background-size: 100% 100%;
 }
-.Scroll-bar::-webkit-scrollbar {
+.detail::-webkit-scrollbar {
     /*滚动条整体样式*/
     width: 8px;
     /*高宽分别对应横竖滚动条的尺寸*/
     height: 8px;
 }
-.Scroll-bar::-webkit-scrollbar-thumb {
+.detail::-webkit-scrollbar-thumb {
     /*滚动条里面小方块*/
     border-radius: 5px;
     -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.1);
     background: rgba(0, 0, 0, 0.1);
 }
-.Scroll-bar::-webkit-scrollbar-track {
+.detail::-webkit-scrollbar-track {
     /*滚动条里面轨道*/
     -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.1);
     border-radius: 0;
