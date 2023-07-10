3ae76c2c7e5398efc34ad634809304b27ed86a3c
Tony Yuan <tony.yuan@coolkit.cn>
2023-07-10 16:40:10 +0800

    Update addon app name

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ cat << EOF > ./packages/server/.env
 LOG_LEVEL=debug
 CONFIG_DATA_PATH=$SERVER_DATA_PATH
 CONFIG_CUBE_HOST=ihost.local
-APP_NAME="Weather forecast"
+APP_NAME="tile2cube-weatherapi"
 ENABLE_PRINT_BUILDINFO=0
 TEST_WEATHER_API_KEY=123456
 EOF
